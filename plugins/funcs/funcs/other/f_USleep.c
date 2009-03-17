f85b8d9a56d8121e376b7b1a727ed2d5dfd56545
elven <n@e-ix.net>
2009-03-17 19:08:39 +0000

    nwnx_funcs/USleep(int): fix typo

--- plugins/funcs/funcs/other/f_USleep.c
+++ plugins/funcs/funcs/other/f_USleep.c
@@ -1,7 +1,7 @@
 #include "NWNXFuncs.h"
 #include <unistd.h>
 
-void Func_Sleep (CGameObject *ob, char *value) {
+void Func_USleep (CGameObject *ob, char *value) {
 	unsigned int usec = atoi(value);
 	if (usec < 0)
 		usec = 0;
