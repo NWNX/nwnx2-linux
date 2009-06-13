2194935e399a66025b3861b4450e04951863045e
zac <zac@intertex.net>
2009-06-13 03:44:34 +0000

    add the ability to get/set the domain spell flag

--- plugins/funcs/funcs/spell/f_SetMemorizedSpell.c
+++ plugins/funcs/funcs/spell/f_SetMemorizedSpell.c
@@ -22,14 +22,14 @@
 
 
 void Func_SetMemorizedSpell (CGameObject *ob, char *value) {
-    int i, sp_class, sp_level, sp_idx, sp_spell, sp_meta, sp_ready;
+    int i, sp_class, sp_level, sp_idx, sp_spell, sp_meta, sp_flags;
     CNWSCreature *cre;
     CNWSStats_Spell *sp;
 
     if (ob == NULL                                    ||
         (cre = ob->vtable->AsNWSCreature(ob)) == NULL ||
         cre->cre_stats == NULL                        ||
-        sscanf(value, "%d %d %d %d %d %d", &sp_class, &sp_level, &sp_idx, &sp_spell, &sp_meta, &sp_ready) != 6 ||
+        sscanf(value, "%d %d %d %d %d %d", &sp_class, &sp_level, &sp_idx, &sp_spell, &sp_meta, &sp_flags) != 6 ||
         sp_level < 0 || sp_level > 9 || sp_idx < 0) {
 
         snprintf(value, strlen(value), "-1");
@@ -58,9 +58,10 @@ void Func_SetMemorizedSpell (CGameObject *ob, char *value) {
                 cre->cre_stats->cs_classes[i].cl_spells_mem[sp_level].data[sp_idx] = sp;
             }
 
-            sp->sp_id    = sp_spell;
-            sp->sp_meta  = sp_meta;
-            sp->sp_ready = sp_ready;
+            sp->sp_id     = sp_spell;
+            sp->sp_meta   = sp_meta;
+            sp->sp_ready  = !!(sp_flags & 1);
+            sp->sp_domain = !!(sp_flags & 2);
         }
 
         return;
