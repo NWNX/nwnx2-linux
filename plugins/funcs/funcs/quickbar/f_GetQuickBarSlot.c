2be6e0b15561945203863a2ef0998f9ceb18de50
James Michael Greenhalgh <orth@layonara.com>
2015-03-26 21:01:24 -0400

    Initial commit to full quickbar support

--- plugins/funcs/funcs/quickbar/f_GetQuickBarSlot.c
+++ plugins/funcs/funcs/quickbar/f_GetQuickBarSlot.c
@@ -16,10 +16,9 @@
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- ***************************************************************************/
+***************************************************************************/
 
 #include "NWNXFuncs.h"
-
 
 void Func_GetQuickBarSlot (CGameObject *ob, char *value) {
     int slot;
@@ -35,11 +34,45 @@ void Func_GetQuickBarSlot (CGameObject *ob, char *value) {
         return;
     }
 
-    snprintf(value, strlen(value), "%d %d %d %d %d", slot,
-        cre->cre_quickbar[slot].qb_type,
-        cre->cre_quickbar[slot].qb_class,
-        cre->cre_quickbar[slot].qb_id,
-        cre->cre_quickbar[slot].qb_metamagic);
+    switch(cre->cre_quickbar[slot].qb_type) {
+        case 1: {
+            snprintf(value, strlen(value), "%d|%x|%x|%d", 
+                cre->cre_quickbar[slot].qb_type,
+                cre->cre_quickbar[slot].qb_objid1,
+                cre->cre_quickbar[slot].qb_objid2,
+                cre->cre_quickbar[slot].qb_id
+            );
+        } 
+        break;
+        case 18: {
+            snprintf(value, strlen(value), "%d|%s|%s|0",
+                cre->cre_quickbar[slot].qb_type,
+                cre->cre_quickbar[slot].qb_label.text,
+                cre->cre_quickbar[slot].qb_command.text
+            );
+        } 
+        break;
+        case 11: case 12: case 13: case 14: case 15: case 16: case 17: {
+            snprintf(value, strlen(value), "%d|%s|%s|0", 
+                cre->cre_quickbar[slot].qb_type,
+                cre->cre_quickbar[slot].qb_label2.text,
+                cre->cre_quickbar[slot].qb_resref
+            );
+        } 
+        break;
+        default: {
+            snprintf(value, strlen(value), "%d|%d|%d|%d",
+                cre->cre_quickbar[slot].qb_type,
+                cre->cre_quickbar[slot].qb_class,
+                cre->cre_quickbar[slot].qb_id,
+                cre->cre_quickbar[slot].qb_metamagic
+            );
+        }
+        break;
+    }
+
+    return;
+
 }
 
 
