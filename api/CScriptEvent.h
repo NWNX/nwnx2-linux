25043087c8436606135e54d158ff0e86cbcf3c0c
jmd <jmd2028@gmail.com>
2016-02-03 06:51:11 -0800

    api: Fill in CScriptEvent

--- api/CScriptEvent.h
+++ api/CScriptEvent.h
@@ -2,6 +2,7 @@
 #define _CSCRIPTEVENT_H_
 #include "nwndef.h"
 #include "CExoString.h"
+#include "CExoArrayList.h"
 
 class CScriptEvent
 {
@@ -22,5 +23,14 @@ public:
     int operator==(CScriptEvent const &) const;
     int operator!=(CScriptEvent const &) const;
 
+    /* 0x0/0 */ unsigned short Type;
+    /* 0x2/2 */ unsigned short field_02;
+    /* 0x04/4 */ CExoArrayList<int> Integers;
+    /* 0x10/16 */ CExoArrayList<float> Floats;
+    /* 0x1C/28 */ CExoArrayList<CExoString> Strings;
+    /* 0x28/40 */ CExoArrayList<nwobjid> Objects;
 };
+
+static_assert_size(CScriptEvent, 0x34);
+
 #endif
