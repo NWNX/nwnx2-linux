57b9d8a405bc268c5d2c72ef290049f7c55305a8
jmd <jmd2028@gmail.com>
2016-03-01 02:30:38 -0800

    api: CNWSpellArray change field name.

--- api/CNWSpellArray.h
+++ api/CNWSpellArray.h
@@ -11,7 +11,7 @@ public:
     CNWSpellArray();
     int operator[](int) const;
 
-    /* 0x0/0 */ unsigned long field_0;
+    /* 0x0/0 */ unsigned long SpellsLength;
     /* 0x4/4 */ CNWSpell *Spells;
 };
 #endif
