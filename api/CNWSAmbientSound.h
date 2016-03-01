4fa3266f24d4b2e7f5025d4e8a981e86d2c869a4
jmd <jmd2028@gmail.com>
2016-03-01 02:31:43 -0800

    api: CNWSAmbientSound fill in fields

--- api/CNWSAmbientSound.h
+++ api/CNWSAmbientSound.h
@@ -24,5 +24,23 @@ public:
     ~CNWSAmbientSound();
     CNWSAmbientSound(unsigned long);
 
+    /* 0x0/0 */ unsigned long Playing;
+    /* 0x4/4 */ unsigned long MusicDelay;
+    /* 0x8/8 */ unsigned long MusicDay;
+    /* 0xC/12 */ unsigned long MusicNight;
+    /* 0x10/16 */ unsigned long field_10;
+    /* 0x14/20 */ unsigned long MusicBattle;
+    /* 0x18/24 */ unsigned long field_18;
+    /* 0x1C/28 */ unsigned long AmbientSndDay;
+    /* 0x20/32 */ unsigned long AmbientSndNight;
+    /* 0x24/36 */ unsigned char AmbientSndDayVol;
+    /* 0x25/37 */ unsigned char AmbientSndNightVol;
+    /* 0x26/38 */ unsigned char field_26;
+    /* 0x27/39 */ unsigned char field_27;
+    /* 0x28/40 */ unsigned long field_28;
+    /* 0x2C/44 */ unsigned long field_2C;
+
 };
+
+static_assert_size(CNWSAmbientSound, 0x30);
 #endif
